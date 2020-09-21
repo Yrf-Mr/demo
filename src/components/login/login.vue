e5f1c091c94308c4423cb72f530dc68fce4e97e9
袁若锋 <674451210@qq.com>
2020-09-21 21:59:43 +0800

    版本4

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -47,7 +47,7 @@ export default {
     methods: {
         // 登录发送请求
         login: function () {
-            console.log(9999, "你好，版本3")
+            console.log(9999, "你好，版本4")
             this.$router.push('/home')
         }
     }
